d8a211ded55bbfc76acd658acd51d04a8f12f639
hide-tech <hidetechyy@gmail.com>
2022-10-24 19:37:37 +0500

    add user table to admin page

--- client/src/components/admin/AdminPage.jsx
+++ client/src/components/admin/AdminPage.jsx
@@ -1,20 +1,45 @@
 import UserTable from "./UserTable";
 import { useState } from "react";
+import { useAuth } from "../context/useAuth";
+import { serverApi } from "../server/ServerApi"
 
 const AdminPage = () => {
 
     const [ users, setUsers ] = useState
+    const [ isFailed, setFailed ] = useState
+    const { user } = useAuth
 
     function banUser( userId ) {
-
+        serverApi.banUserById(user, userId)
+        .then(
+            console.log("user with id "+ userId + " has been banned")
+        )
+        .catch(error => {
+            handleLogError(error)
+            setFailed(true)
+        })    
     }
 
     function unbanUser( userId ) {
-
+        serverApi.unbanUserById(user, userId)
+        .then(
+            console.log("user with id "+ userId + " has been unlocked")
+        )
+        .catch(error => {
+            handleLogError(error)
+            setFailed(true)
+        }) 
     }
 
     function makeAdmin( userId ){
-        
+        serverApi.setAdminUser(user, userId)
+        .then(
+            console.log("user with id "+ userId + " has became admin")
+        )
+        .catch(error => {
+            handleLogError(error)
+            setFailed(true)
+        })
     }
     
     return ( 
