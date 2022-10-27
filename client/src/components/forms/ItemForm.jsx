7c77001ccd4e28f701512a3d2f5ffb28f97b1661
hide-tech <hidetechyy@gmail.com>
2022-10-27 12:35:37 +0500

    finish itemForm

--- client/src/components/forms/ItemForm.jsx
+++ client/src/components/forms/ItemForm.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 const ItemForm = ({changedItem, actionFunction}) => {
     
     const [ item, setItem ] = useState(changedItem)
+    const [ tags, setTags ] = useState(changedItem.tags)
+    const [ fields, setFields ] = useState(changedItem.fields)
     const navigate = useNavigate()
 
     let tagsTable
@@ -11,7 +13,10 @@ const ItemForm = ({changedItem, actionFunction}) => {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-
+        const newItem = item
+        newItem.tags = tags
+        newItem.fields = fields
+        setItem(newItem)
         actionFunction(item)
         navigate(-1)
     }
@@ -22,46 +27,83 @@ const ItemForm = ({changedItem, actionFunction}) => {
         setItem(newItem)
     }
 
-    const handleChangeTags = (event) => {
-        const newItem = item
-        newItem.tags = event.target.tags.value
-        setItem(newItem)
+    const handleChangeTags = (index, e) => {
+        const tagname = e.target.tag.value
+        const tagList = tags
+        const tagEdit = tagList[index]
+        tagEdit.name = tagname
+        tagList[index] = tagEdit
+        setTags(tagList)
     }
 
-    const handleChangeFields = (event) => {
-        const newItem = item
-        newItem.fields = event.target.fields.value
-        setItem(newItem)
+    const handleChangeFieldKey = (key, e) => {
+        const fieldKey = e.target.fieldK.value
+        const fieldz = fields
+        fieldz[fieldKey] = fieldz[key]
+        delete fieldz[key]
+        setFields(fieldz)
     }
 
-    if (item.tags.length == 0){
+    const handleChangeFieldValue = (key, e) => {
+        const fieldValue = e.target.fieldV.value
+        const fieldz = fields
+        fieldz[key] = fieldValue
+        setFields(fieldz)
+    }
+
+    if (tags.length == 0){
         tagsTable = <div key='no-tags'>No Tags</div>
     } else {
-        tagsTable = item.tags.map(tag => {
+        tagsTable = tags.map((tag, index) => {
             return (
-                <label>
-                    Tags: <input type="text" name="tag" onChange={handleChangeTags}>{tag.name}</input>
-                </label>
+                <>
+                    <label>
+                        Tags: <input type="text" name="tag" onChange={e => handleChangeTags(index)}>{tag.name}</input>
+                        <button type="button" onClick={e => handleDeleteTag(index)}>Delete tag</button>
+                    </label> 
+                </>
             )
         })
     }
 
     const handleAddNewTag = (event) => {
-        
+        const newTags = tags
+        newTags.push({ 'name':'' })
+        setTags(newTags)
+    }
+
+    const handleDeleteTag = (index, e) => {
+        const elem = tags[index]
+        const newTags = tags.filter(el => el !== elem)
+        setTags(newTags)
     }
     
-    if (item.fields.length == 0){
+    if (fields.length == 0){
         fieldsTable = <div key='no-fields'>No Fields</div>
     } else {
-        fieldsTable = item.fields.map(field => {
+        fieldsTable = Object.entries(fields).map((fieldKey, fieldValue) => {
             return (
-                <div>Fields</div>
+                <>
+                    <label>
+                        <input type='text' name="fieldK" onChange={e => handleChangeFieldKey(fieldKey)}>{fieldKey}</input>: 
+                        <input type="text" name="fieldV" onChange={e => handleChangeFieldValue(fieldKey)}>{fieldValue}</input>
+                        <button type="button" onClick={e => handleDeleteField(fieldKey)}>Delete field</button>
+                    </label> 
+                </>
             )
         })
     }
 
     const handleAddNewField = (event) => {
-        
+        const newFields = fields
+        newFields[''] = ''
+        setFields(newFields)
+    }
+
+    const handleDeleteField = (key, e) => {
+        const newFields = fields
+        delete newFields[key]
+        setFields(newFields)
     }
     
     return ( 
@@ -73,10 +115,10 @@ const ItemForm = ({changedItem, actionFunction}) => {
                 </label>
                 <br />
                 {tagsTable}
-                <button onClick={handleAddNewTag}>Add new tag</button>
+                <button type="button" onClick={handleAddNewTag}>Add new tag</button>
                 <br />
                 {fieldsTable}
-                <button onClick={handleAddNewField}>Add new field</button>
+                <button type="button" onClick={handleAddNewField}>Add new field</button>
             </form>
         </div>
     );
